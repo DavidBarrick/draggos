7b8d46700fde0c542b60a0ed83651abb2200fefc
David Barrick <david@barrickdevelopment.com>
2022-02-19 15:11:03 -0300

    install deps

---
+++ app/src/Landing/index.js
@@ -0,0 +1,14 @@
+import logo from '../logo.svg';
+
+const Landing = () => {
+    return (
+        <div className="App">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                <code>draggos 🐲</code>
+            </p>
+        </div>
+    );
+  };
+  
+  export default Landing;
